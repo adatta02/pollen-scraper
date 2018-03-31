27a42737ac4e2cce38d12aea4bd93341bb35f39b
Ashish Datta <ashish@setfive.com>
2018-03-31 16:27:41 -0400

    Wiring up more scraping and output

--- src/renderer.ts
+++ src/renderer.ts
@@ -5,6 +5,7 @@ import * as cheerio from "cheerio";
 import * as _ from "lodash";
 import * as fs from "fs";
 import * as moment from "moment";
+import * as async from "async";
 import {Globals} from "./main";
 import WriteStream = NodeJS.WriteStream;
 
@@ -55,6 +56,7 @@ class JPPollenComponent implements ng.IComponentOptions {
 
 interface PollenData {
     status : string;
+    date : string;
     sum : number;
     points : number[];
 }
@@ -75,13 +77,19 @@ class JPPollenController {
 
     public tab : "list" | "progress" | "none" = "none";
 
+    public dateArray : string[] = [];
     public cities : City[] = [];
-    public loading : boolean = false;
-    public displayError : string = null;
 
-    public overallProgress : number = 0;
+    public loading : boolean = false;
+    public displayError : string = "";
+    public selectedOutputFile : string = "/home/ashish/Downloads/jp_output.csv";
+
     public startDate : Date = moment().subtract(2, "month").toDate();
     public endDate : Date = new Date();
+
+    public numRequests : number = 0;
+    public requestsMade : number = 0;
+    public overallProgress : number = 0;
 
     constructor($rootScope : angular.IRootScopeService){
         this.$rootScope = $rootScope;
@@ -125,6 +133,7 @@ class JPPollenController {
                 this.doFetch(results);
             })
             .catch(err => {
+                console.log(err);
                 this.setLoading(false);
                 this.setDisplayError("Invalid 'Start Date'. Maybe its too far in the past?");
             });
@@ -145,17 +154,72 @@ class JPPollenController {
             d.add(1, "day");
         }
 
-        const zeroData = cities.map(f => {
-            f.data = dateArray.map(d => { return {status: "new", sum: 0, points: []}; });
-            return f;
-        });
+        const cityDateRequest = _.flatten(cities.map(f => {
+            return dateArray.map(d => { return {city: f, date: d}; });
+        }));
+
+        this.dateArray = dateArray;
 
         this.$rootScope.$applyAsync(() => {
-            this.overallProgress = 0;
-            this.cities = zeroData;
+            this.numRequests = cityDateRequest.length;
+            this.requestsMade = 0;
+            this.setProgress(0);
+
+            this.cities = cities.map(f => {
+                f.data = dateArray.map(d => { return {status: "new", sum: 0, date: d, points: []}; });
+                return f;
+            });
+
+            async.eachLimit(cityDateRequest, 5, (req, callback) => {
+
+                const targetCity = this.cities.find(f => f == req.city);
+                const targetDate = targetCity.data.find(f => f.date == req.date);
+
+                const url = `http://weathernews.jp/pollen/xml/${req.city.id}/${req.date}.xml`;
+
+                got(url)
+                .then(response => {
+                    const xml$ = cheerio.load(response.body);
+                    const pollen = xml$("pollen").text();
+
+                    this.$rootScope.$applyAsync(() => {
+                        targetDate.status = "done";
+                    });
+
+                    if(pollen){
+                        targetDate.points = pollen.split(",").map(f => parseInt(f));
+                        targetDate.sum = _.sum(targetDate.points);
+                    }
+
+                    this.writeData();
+                    callback();
+                })
+                .catch(err => {
+                    this.$rootScope.$applyAsync(() => {
+                        targetDate.status = "error";
+                    });
+                    callback();
+                });
+
+                this.requestsMade += 1;
+                const progress = Math.ceil((this.requestsMade / this.numRequests) * 100);
+                this.setProgress(progress);
+            }, (err) => {
+                if(err) {
+                    this.setDisplayError(<string> err);
+                    return;
+                }
+
+            });
+
         });
 
-        
+    }
+
+    private setProgress(val : number) : void {
+        this.$rootScope.$applyAsync(() => {
+            this.overallProgress = val;
+        });
     }
 
     public fetchCities() : void {
@@ -196,6 +260,27 @@ class JPPollenController {
         });
     }
 
+    public selectFile() : void {
+        dialog.showSaveDialog(null, null, (result) => {
+            this.$rootScope.$applyAsync(() => {
+               this.selectedOutputFile = result;
+            });
+        });
+    }
+
+    private writeData() : void {
+        const header = ["Id", "Name"].concat(this.dateArray);
+        const writer : WriteStream = csv({ headers: header });
+
+        writer.pipe(fs.createWriteStream(this.selectedOutputFile));
+        this.cities.forEach(f => {
+            const sums = f.data.map(d => d.sum.toString());
+            const data = [f.id, f.name].concat(sums);
+            writer.write((<any> data));
+        });
+        writer.end();
+    }
+
     public downloadCSV() : void {
 
         dialog.showSaveDialog(null, null, (result) => {
@@ -203,7 +288,7 @@ class JPPollenController {
                 return;
             }
 
-            const writer : WriteStream = csv({ headers: ["Id", "Name"]})
+            const writer : WriteStream = csv({ headers: ["Id", "Name"]});
             writer.pipe(fs.createWriteStream(result));
             this.cities.map(f => [f.id, f.name]).forEach(f => {
                 writer.write((<any> f));
